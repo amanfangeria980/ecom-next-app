ccf239d6100cfcf3f06df202a9b5550fef635f29
Aman Fangeria <aman.fangeria@geekyants.com>
2024-10-01 12:07:07 +0530

    feat: add Header and SearchBar components

---
+++ components/Header.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { Grid2X2, Heart, LayoutGrid, ShoppingCart, User } from "lucide-react";
+import SearchBar from "./SearchBar";
+const Header = () => {
+    return (
+        <header className="flex flex-col md:flex-row bg-ecom px-10 py-7 space-x-5 items-center">
+            <Link href={"/"} className="mb-5 md:mb-0">
+                <Image
+                    src="https://i.imgur.com/5V4wehM.png"
+                    alt="Logo"
+                    width={150}
+                    height={150}
+                />
+            </Link>
+            <SearchBar />
+
+            <div className="flex mt-5 md:mt-0 space-x-5">
+                <Link
+                    href={"/"}
+                    className="hidden xl:flex text-white font-bold items-center space-x-2 text-sm"
+                >
+                    <Grid2X2 size={20} />
+                    <p>Departments</p>
+                </Link>
+                <Link
+                    href={"/"}
+                    className="hidden xl:flex text-white font-bold items-center space-x-2 text-sm"
+                >
+                    <LayoutGrid size={20} />
+                    <p>Services</p>
+                </Link>
+                <Link
+                    href={"/"}
+                    className="flex text-white font-bold items-center space-x-2 text-sm"
+                >
+                    <Heart size={20} />
+                    <div>
+                        <p className="text-xs font-extralight">Reorder</p>
+                        <p>My Items</p>
+                    </div>
+                </Link>
+                <Link
+                    href={"/"}
+                    className="flex text-white font-bold items-center space-x-2 text-sm"
+                >
+                    <User size={20} />
+                    <div>
+                        <p className="text-xs font-extralight">Sign In</p>
+                        <p>Account</p>
+                    </div>
+                </Link>
+                <Link
+                    href={"/basket"}
+                    className="flex text-white font-bold items-center space-x-2 text-sm"
+                >
+                    <ShoppingCart size={20} />
+                    <div>
+                        <p className="text-xs font-extralight">No Items</p>
+                        <p>$0.00</p>
+                    </div>
+                </Link>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
---
+++ components/SearchBar.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { Search } from "lucide-react";
+import { useRouter } from "next/navigation";
+import React, { FormEvent, useState } from "react";
+
+const SearchBar = () => {
+    const [searchInput, setSearchInput] = useState("");
+    const router = useRouter();
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (searchInput) router.push(`/search/${searchInput}`);
+    };
+    return (
+        <form
+            className="flex items-center bg-white rounded-full w-full flex-1"
+            onSubmit={handleSubmit}
+        >
+            <input
+                type="text"
+                placeholder="Search Everything..."
+                className="flex-1 px-4 rounded-l-full outline-none placeholder:text-sm text-black"
+                value={searchInput}
+                onChange={(e) => setSearchInput(e.target.value)}
+            />
+            <button type="submit">
+                <Search className="h-10 rounded-full px-2 w-10 bg-yellow-400 " />
+            </button>
+        </form>
+    );
+};
+
+export default SearchBar;
