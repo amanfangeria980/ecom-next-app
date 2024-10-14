10c0938f9476fe9596af5151512fa85187c164c8
Aman Fangeria <aman.fangeria@geekyants.com>
2024-10-14 19:11:37 +0530

    feat: add fetchProduct.ts to fetch product

---
+++ lib/fetchProduct.ts
@@ -0,0 +1,25 @@
+import { ProductTypings } from "./productTypings";
+
+async function fetchProduct(id: string) {
+    try {
+        const key = process.env.SERP_API_KEY;
+        // const newUrl = new URL(
+        //     `https://serpapi.com/search.json?api_key=${key}&engine=walmart_product&product_id=${id}`
+        // );
+        const newUrl = "http://localhost:3002/data";
+        const res = await fetch(newUrl, {
+            next: {
+                revalidate: 60 * 60,
+            },
+        });
+        if (!res.ok) throw new Error(`Error: ${res.statusText}`);
+        const json = await res.json();
+        const data: ProductTypings = json;
+        return data;
+    } catch (err) {
+        console.error("Some error occured");
+        throw err;
+    }
+}
+
+export default fetchProduct;
